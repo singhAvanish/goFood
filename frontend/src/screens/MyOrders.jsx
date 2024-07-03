1dbd2703d1a5adcf2d961a43160e8869338cd9a3
singhAvanish <88013566+singhAvanish@users.noreply.github.com>
2024-07-03 15:44:56 +0530

    Update MyOrders.jsx

--- frontend/src/screens/MyOrders.jsx
+++ frontend/src/screens/MyOrders.jsx
@@ -7,7 +7,7 @@ export default function MyOrders() {
 
   const fetchMyOrder = async () => {
     try {
-      const response = await fetch("http://localhost:4000/api/myOrderData", {
+      const response = await fetch("https://gofood-nc4b.onrender.com/api/myOrderData", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
