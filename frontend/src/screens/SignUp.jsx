5fd41b7a4d10b2f0725c0197cf4c47bee207af85
singhAvanish <88013566+singhAvanish@users.noreply.github.com>
2024-07-03 15:45:15 +0530

    Update SignUp.jsx

--- frontend/src/screens/SignUp.jsx
+++ frontend/src/screens/SignUp.jsx
@@ -6,7 +6,7 @@ export default function SignUp() {
     const navigate=useNavigate();
     const handelSubmit=async(e)=>{
         e.preventDefault();
-        const response = await fetch("http://localhost:4000/api/createuser",{
+        const response = await fetch("https://gofood-nc4b.onrender.com/api/createuser",{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
